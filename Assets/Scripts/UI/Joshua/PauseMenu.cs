727004d795330b351f57cb8d196c2e484430c70d
codeloco07 <joshuaver.delapena@gmail.com>
2023-07-14 18:16:55 +0800

    UI Update
    - Pause Menu Functional
    - Sound

---
+++ Assets/Scripts/UI/Joshua/PauseMenu.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool GameisPaused = false;
+
+    public GameObject pauseMenuUI;
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameisPaused)
+            {
+                Resume();
+
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameisPaused = false;
+        Debug.Log("Resume");
+    }
+
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameisPaused = true;
+        Debug.Log("Paused");
+    }
+
+    public void exit2Menu()
+    {
+        Debug.Log("Exit to Menu");
+    }
+
+    
+}
