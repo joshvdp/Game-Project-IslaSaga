74ced2ffeacc485767f8b675631b1d05ce448b19
Laurenito <richmond2.vergara@neu.edu.ph>
2023-07-16 01:17:49 +0800

    InGameUI added to Main Scene, Options Menu Bug Fixed

--- Assets/Scripts/UI/Joshua/MainMenu.cs
+++ Assets/Scripts/UI/Joshua/MainMenu.cs
@@ -9,6 +9,7 @@ public class MainMenu : MonoBehaviour
     public void PlayGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneManager.LoadScene(2, LoadSceneMode.Additive);
     }
 
     public void exit()
--- Assets/Scripts/UI/Joshua/PauseMenu.cs
+++ Assets/Scripts/UI/Joshua/PauseMenu.cs
@@ -7,6 +7,8 @@ public class PauseMenu : MonoBehaviour
     public static bool GameisPaused = false;
 
     public GameObject pauseMenuUI;
+
+    public GameObject optionsMenuUI;
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape))
@@ -25,6 +27,7 @@ public class PauseMenu : MonoBehaviour
     public void Resume()
     {
         pauseMenuUI.SetActive(false);
+        optionsMenuUI.SetActive(false);
         Time.timeScale = 1f;
         GameisPaused = false;
         Debug.Log("Resume");
