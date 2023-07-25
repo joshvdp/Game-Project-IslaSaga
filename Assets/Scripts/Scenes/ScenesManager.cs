fd6fff450170c4fc682394c5d6568944df06375f
codeloco07 <joshuaver.delapena@gmail.com>
2023-07-25 14:20:50 +0800

    Scene Transition
    - Added Fade-In/Fade-Out when switching scenes

--- Assets/Scripts/Scenes/ScenesManager.cs
+++ Assets/Scripts/Scenes/ScenesManager.cs
@@ -2,15 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
 namespace Manager
 {
     public class ScenesManager : MonoBehaviour
     {
+        [SerializeField] Animator transitionAnim;
+        public void Start()
+        {
+            DontDestroyOnLoad(this.gameObject);
+        }
+
         public void LoadScene()
         {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            StartCoroutine(loadNow());
         }
+        IEnumerator loadNow()
+        {
+            transitionAnim.SetTrigger("End");
+            yield return new WaitForSeconds(1);
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            transitionAnim.SetTrigger("Start");
+        }
+
         public void exit()
         {
             Debug.Log("Quit");
