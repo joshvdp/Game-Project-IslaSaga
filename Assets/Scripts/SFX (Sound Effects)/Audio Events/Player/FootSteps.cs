0714fa306df6a0e881889455f75c2d2cdef8902d
@joshvdp <joshuaver.delapena@gmail.com>
2023-09-26 00:47:41 +0800

    Improved Foot Steps Sound
    - Added Different Variants of Foot Steps Assets
    - Fix Some AnimEvents

--- Assets/Scripts/SFX (Sound Effects)/Audio Events/Player/FootSteps.cs
+++ Assets/Scripts/SFX (Sound Effects)/Audio Events/Player/FootSteps.cs
@@ -3,30 +3,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FootSteps : MonoBehaviour
+namespace AudioSoundEvents
 {
-    [SerializeField] 
-    public AudioClip[] clips;
+    public class FootSteps : MonoBehaviour
+    {
+        [SerializeField] 
+        public AudioClip[] clips;
 
-    public AudioSource audioSource;
-    public void Awake()
-    {
-        audioSource = GetComponent<AudioSource>();
-    }
-    public void Step()
-    {
-        AudioClip clip = GetRandomClip();
-        audioSource.PlayOneShot(clip);
-        Debug.Log(clip);
-    }
+        public AudioSource audioSource;
+        public void Awake()
+        {
+            audioSource = GetComponent<AudioSource>();
+        }
+        public void Step()
+        {
+            AudioClip clip = GetRandomClip();
+            audioSource.PlayOneShot(clip);
+            Debug.Log(clip);
+        }
 
-    public void RunStep()
-    {
-        
-    }
+        public void RunStep()
+        {
+            Debug.Log("runSteps");
+        }
 
-    public AudioClip GetRandomClip()
-    {
-        return clips[UnityEngine.Random.Range(0, clips.Length)];
+        public AudioClip GetRandomClip()
+        {
+            return clips[UnityEngine.Random.Range(0, clips.Length)];
+        }
     }
 }
+
