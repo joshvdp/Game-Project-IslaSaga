c5352a7486dfbf27081ce1a82d50095f198d52cf
Laurenito <richmond2.vergara@neu.edu.ph>
2023-10-09 15:27:08 +0800

    Dialogue Box
    - Added Dialogue box
    - Added new Fonts
    - Fixed positions of In Game buttons.

---
+++ Assets/Scripts/DialogueSystem/DialogueHolder.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DialogueSystem
+{
+    public class DialogueHolder : MonoBehaviour
+    {
+        private IEnumerator dialogueSeq;
+        private void Awake()
+        {
+            dialogueSeq = dialogueSequence();
+            StartCoroutine(dialogueSeq);
+        }
+        private IEnumerator dialogueSequence()
+        {
+            for (int i = 0; i < transform.childCount; i++)
+            {
+                Deactivate();
+                transform.GetChild(i).gameObject.SetActive(true);
+                yield return new WaitUntil(() => transform.GetChild(i).GetComponent<DialogueLine>().Finished);
+            }
+            gameObject.SetActive(false);
+        }
+
+        private void Deactivate()
+        {
+            for (int i = 0; i < transform.childCount; i++)
+            {
+                transform.GetChild(i).gameObject.SetActive(false);
+            }
+        }
+    }
+}
+
