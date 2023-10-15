21824bb1adb4e25a2f99658f663dc88e72a4805c
Laurenito <richmond2.vergara@neu.edu.ph>
2023-10-16 04:24:20 +0800

    Control Tutorial Pop-up
    - Added a tutorial for the Controls in the game
    - Added a Controls Menu

--- Assets/Scripts/Pamesa/DialogueTrigger.cs
+++ Assets/Scripts/Pamesa/DialogueTrigger.cs
@@ -7,12 +7,18 @@ public class DialogueTrigger : MonoBehaviour
 {
 
     public int dialogueIndex;
+    public int controlIndex;
 
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.collider.name == "Moveable Box")
         {
             DialogueHandler.Instance.EnableDialogue(dialogueIndex);
+        }
+
+        if (collision.collider.name == "Dialogue Collider")
+        {
+            DialogueHandler.Instance.EnableDialogue(controlIndex);
         }
     }
 }
