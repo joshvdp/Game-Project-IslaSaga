3fd1904392a62308079071ccb67f31f2eec81238
Ayney <cloudthekidrs111@gmail.com>
2023-11-21 08:43:50 +0800

    Connecting UI variables for scene load

--- Assets/Scripts/DialogueSystem/Story/StoryText.cs
+++ Assets/Scripts/DialogueSystem/Story/StoryText.cs
@@ -28,123 +28,128 @@ namespace Story
 
         [SerializeField] TextMeshProUGUI mainText;
         [SerializeField] bool isLooping = false;
+        [SerializeField] string NextSceneName;
+
+        [SerializeField] float TextSpeed;
+        [SerializeField] Vector3 StartingPostion;
+        [SerializeField] float EndPosition;
         private void Start()
         {
             string activeSceneName = SceneManager.GetActiveScene().name;
             myGorectTransform = gameObject.GetComponent<RectTransform>();
-            
-            
-            switch (activeSceneName)
-            {
-                case "StoryScene":
-                    StartCoroutine(Story());
-                    break;
-                case "AfterLevel":
-                    StartCoroutine(AfterLevel());
-                    break;
-                case "EndScene":
-                    StartCoroutine(End());
-                    break;
-            }
+
+            //Debug.Log("ACTIVE SCENE NAME IS " + activeSceneName);
+            //switch (activeSceneName)
+            //{
+            //    case "StoryScene":
+            //        StartCoroutine(Story());
+            //        break;
+            //    case "AfterLevel":
+            //        StartCoroutine(AfterLevel());
+            //        break;
+            //    case "EndScene":
+            //        StartCoroutine(End());
+            //        break;
+            //}
 
             StartCoroutine(ShowSkipButton());
         }
 
-        IEnumerator Story()
-        {
-            switch (PlatformType)
-                {
-                    case UIPlatformType.Mobile:
-                        textSpeed = 45.0f;
-                        myGorectTransform.transform.localPosition = new Vector3(0f, -1231f, 0f);
-                        textEndPosition = 1232f;
-                        break;
-                }
-            while (myGorectTransform.localPosition.y < textEndPosition)
-            {
+        //IEnumerator Story()
+        //{
+        //    switch (PlatformType)
+        //        {
+        //            case UIPlatformType.Mobile:
+        //                textSpeed = 45.0f;
+        //                myGorectTransform.transform.localPosition = StartingPostion;
+        //                textEndPosition = EndPosition;
+        //                break;
+        //        }
+        //    while (myGorectTransform.localPosition.y < textEndPosition)
+        //    {
                 
-                myGorectTransform.Translate(Vector3.up * textSpeed * Time.deltaTime);
-                if (myGorectTransform.localPosition.y > textEndPosition)
-                {
-                    if (isLooping)
-                    {
-                        myGorectTransform.localPosition = Vector3.up * textBeginPosition;
-                    }
-                    else
-                    {
-                        SceneLoader.Instance.LoadNextSceneAsync(SceneLoader.Instance.SceneName.Level1);
-                        break;
-                    }
-                }
-                yield return null;
-            }
+        //        myGorectTransform.Translate(Vector3.up * textSpeed * Time.deltaTime);
+        //        if (myGorectTransform.localPosition.y > textEndPosition)
+        //        {
+        //            if (isLooping)
+        //            {
+        //                myGorectTransform.localPosition = Vector3.up * textBeginPosition;
+        //            }
+        //            else
+        //            {
+        //                SceneLoader.Instance.LoadNextSceneAsync(SceneLoader.Instance.SceneName.Level1);
+        //                break;
+        //            }
+        //        }
+        //        yield return null;
+        //    }
 
             
-        }
+        //}
 
-        IEnumerator AfterLevel()
-        {
-            switch (PlatformType)
-            {
-                case UIPlatformType.Mobile:
-                    textSpeed = 80f;
-                    myGorectTransform.transform.localPosition = new Vector3(0f, -524, 0f);
-                    textEndPosition = 525f;
-                    break;
-            }
-            while (myGorectTransform.localPosition.y < textEndPosition)
-            {
+        //IEnumerator AfterLevel()
+        //{
+        //    switch (PlatformType)
+        //    {
+        //        case UIPlatformType.Mobile:
+        //            textSpeed = 80f;
+        //            myGorectTransform.transform.localPosition = new Vector3(0f, -524, 0f);
+        //            textEndPosition = 525f;
+        //            break;
+        //    }
+        //    while (myGorectTransform.localPosition.y < textEndPosition)
+        //    {
 
-                myGorectTransform.Translate(Vector3.up * textSpeed * Time.deltaTime);
-                if (myGorectTransform.localPosition.y > textEndPosition)
-                {
-                    if (isLooping)
-                    {
-                        myGorectTransform.localPosition = Vector3.up * textBeginPosition;
-                    }
-                    else
-                    {
-                        SceneLoader.Instance.LoadNextSceneAsync(SceneLoader.Instance.SceneName.Level1);
-                        break;
-                    }
-                }
-                yield return null;
-            }
-
-
-        }
-
-        IEnumerator End()
-        {
-            switch (PlatformType)
-            {
-                case UIPlatformType.Mobile:
-                    textSpeed = 45.0f;
-                    myGorectTransform.transform.localPosition = new Vector3(0f, -862.974f, 0f);
-                    textEndPosition = 930f;
-                    break;
-            }
-            while (myGorectTransform.localPosition.y < textEndPosition)
-            {
-
-                myGorectTransform.Translate(Vector3.up * textSpeed * Time.deltaTime);
-                if (myGorectTransform.localPosition.y > textEndPosition)
-                {
-                    if (isLooping)
-                    {
-                        myGorectTransform.localPosition = Vector3.up * textBeginPosition;
-                    }
-                    else
-                    {
-                        SceneLoader.Instance.LoadNextSceneAsync(SceneLoader.Instance.SceneName.Level1);
-                        break;
-                    }
-                }
-                yield return null;
-            }
+        //        myGorectTransform.Translate(Vector3.up * textSpeed * Time.deltaTime);
+        //        if (myGorectTransform.localPosition.y > textEndPosition)
+        //        {
+        //            if (isLooping)
+        //            {
+        //                myGorectTransform.localPosition = Vector3.up * textBeginPosition;
+        //            }
+        //            else
+        //            {
+        //                SceneLoader.Instance.LoadNextSceneAsync(SceneLoader.Instance.SceneName.EndScene);
+        //                break;
+        //            }
+        //        }
+        //        yield return null;
+        //    }
 
 
-        }
+        //}
+
+        //IEnumerator End()
+        //{
+        //    switch (PlatformType)
+        //    {
+        //        case UIPlatformType.Mobile:
+        //            textSpeed = 45.0f;
+        //            myGorectTransform.transform.localPosition = new Vector3(0f, -862.974f, 0f);
+        //            textEndPosition = 930f;
+        //            break;
+        //    }
+        //    while (myGorectTransform.localPosition.y < textEndPosition)
+        //    {
+
+        //        myGorectTransform.Translate(Vector3.up * textSpeed * Time.deltaTime);
+        //        if (myGorectTransform.localPosition.y > textEndPosition)
+        //        {
+        //            if (isLooping)
+        //            {
+        //                myGorectTransform.localPosition = Vector3.up * textBeginPosition;
+        //            }
+        //            else
+        //            {
+        //                SceneLoader.Instance.LoadNextSceneAsync(SceneLoader.Instance.SceneName.EndScene);
+        //                break;
+        //            }
+        //        }
+        //        yield return null;
+        //    }
+
+
+        //}
 
 
 
@@ -165,30 +170,50 @@ namespace Story
 
         private void Update()
         {
+            ScrollText();
             string activeSceneName = SceneManager.GetActiveScene().name;
             if (Input.GetMouseButtonDown(0))
             {
 
-                switch (activeSceneName)
-                {
-                    case "StoryScene":
-                        StopCoroutine(Story());
-                        break;
-                    case "AfterLevel":
-                        StopCoroutine(AfterLevel());
-                        break;
-                    case "EndScene":
-                        StopCoroutine(End());
-                        break;
+                //switch (activeSceneName)
+                //{
+                //    case "StoryScene":
+                //        StopCoroutine(Story());
+                //        break;
+                //    case "AfterLevel":
+                //        StopCoroutine(AfterLevel());
+                //        break;
+                //    case "EndScene":
+                //        StopCoroutine(End());
+                //        break;
                         
-                }
+                //}
 
-                SceneLoader.Instance.LoadNextSceneAsync(SceneLoader.Instance.SceneName.Level1);
-                
+                SceneLoader.Instance.LoadNextSceneAsync(NextSceneName);
             }
         }
 
+        void ScrollText()
+        {
+            if(myGorectTransform.localPosition.y < textEndPosition)
+            {
 
+                myGorectTransform.Translate(Vector3.up * TextSpeed * Time.deltaTime);
+                if (myGorectTransform.localPosition.y > textEndPosition)
+                {
+                    if (isLooping)
+                    {
+                        myGorectTransform.localPosition = Vector3.up * textBeginPosition;
+                    }
+                    else
+                    {
+                        SceneLoader.Instance.LoadNextSceneAsync(NextSceneName);
+                        
+                    }
+                }
+                
+            }
+        }
 
 
 
