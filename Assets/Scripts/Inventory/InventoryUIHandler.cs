0c6d90aa79e7d0ab897e74f1c1041335348e7887
FrustratedJeh <80657334+novalostj@users.noreply.github.com>
2024-06-17 00:23:24 +0800

    Auto Equip on pickup

--- Assets/Scripts/Inventory/InventoryUIHandler.cs
+++ Assets/Scripts/Inventory/InventoryUIHandler.cs
@@ -82,15 +82,34 @@ public class InventoryUIHandler : MonoBehaviour
     }
     public void AddItemToUI(InventoryItem Item)
     {
-        for (int i = 0; i < ItemSlots.Count; i++)
+        InventoryItem cache = Item;
+        if (Item.EquippableType == EquippableItemType.WEAPON)
         {
-            if (!ItemSlots[i].IsOccupied)
+            cache = WeaponSlot.ItemData;
+            WeaponSlot.RemoveItemFromSlot();
+            WeaponSlot.ItemData = Item;
+            WeaponSlot.InitializeItem();
+        }
+        else if (Item.EquippableType == EquippableItemType.SHIELD)
+        {
+            cache = ShieldSlot.ItemData;
+            ShieldSlot.RemoveItemFromSlot();
+            ShieldSlot.ItemData = Item;
+            ShieldSlot.InitializeItem();
+        }
+        if (cache != null)
+        {
+            foreach (PlayerInventorySlot itemSlot in ItemSlots)
             {
-                ItemSlots[i].ItemData = Item;
-                ItemSlots[i].InitializeItem();
+                if (itemSlot.IsOccupied)
+                    continue;
+                
+                itemSlot.ItemData = cache;
+                itemSlot.InitializeItem();
                 break;
             }
         }
+        
         CheckIfPotionIsAvailable();
         InventoryData.UpdateInventoryUI(ItemSlots);
     }
@@ -105,6 +124,7 @@ public class InventoryUIHandler : MonoBehaviour
                 break;
             }
         }
+        
         CheckIfPotionIsAvailable();
         InventoryData.UpdateInventoryUI(ItemSlots);
     }
