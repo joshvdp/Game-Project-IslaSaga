ce6c2951f4d841dbcfa30a9637464063791b86f4
Ayney <cloudthekidrs111@gmail.com>
2024-07-20 23:31:50 +0800

    Added randomized explosion position for broken vase, and adjusted vase positions in Level 1

--- Assets/Scripts/Core/AddForceOnSpot.cs
+++ Assets/Scripts/Core/AddForceOnSpot.cs
@@ -8,13 +8,15 @@ public class AddForceOnSpot : MonoBehaviour
     [SerializeField] float Radius;
     [SerializeField] float Torque;
     [SerializeField] Transform ForcePosition;
+    [SerializeField] float RandomForcePositionIntensity = 0.2f;
     private void Awake()
     {
         Rigidbody[] rbs = GetComponentsInChildren<Rigidbody>();
-
+        Vector3 RandomizedForcePositionOffset = new Vector3(Random.Range(-RandomForcePositionIntensity, RandomForcePositionIntensity), 0f, Random.Range(-RandomForcePositionIntensity, RandomForcePositionIntensity));
+        Debug.Log(RandomizedForcePositionOffset);
         for (int i = 0; i < rbs.Length; i++)
         {
-            rbs[i].AddExplosionForce(Force, ForcePosition.position, Radius);
+            rbs[i].AddExplosionForce(Force, ForcePosition.position + RandomizedForcePositionOffset, Radius);
             rbs[i].AddTorque(Random.Range(1,Torque), Random.Range(1, Torque), Random.Range(1, Torque));
         }
     }
